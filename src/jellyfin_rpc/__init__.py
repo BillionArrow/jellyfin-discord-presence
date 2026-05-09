2e5138994a95dfa10bb8379b0091786dedd71b2c
BillionArrow <2005emirhanozer@gmail.com>
2026-05-09 16:26:51 +0200

    fix: correct import path from .main to .jellyfin_rpc

--- src/jellyfin_rpc/__init__.py
+++ src/jellyfin_rpc/__init__.py
@@ -1,6 +1,6 @@
 from importlib.metadata import version
 
-from .main import load_config, start_discord_rpc
+from .jellyfin_rpc import load_config, start_discord_rpc
 
 __version__ = version('jellyfin-rpc')
 __all__ = ['load_config', 'start_discord_rpc']
